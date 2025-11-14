888dca1c9982a71ca85ce927bce67d0c4911c6d4
Karma <72810712+Karma666064@users.noreply.github.com>
2025-11-14 13:38:39 +0400

    feat: Moving is good

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
+using Terresquall;
 
 public class PlayerMove : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    [SerializeField] MoveButton[] moveButtons;
+
     [SerializeField] float speed;
     
     public bool canMove = true;
     bool isMoving;
 
-    //public 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            Vector2 direction = new Vector2(
+                VirtualJoystick.GetAxis("Horizontal"),
+                VirtualJoystick.GetAxis("Vertical")
+            ).normalized;
+
+            rb.linearVelocity = direction * speed;
+
+            if (direction.magnitude != 0) isMoving = true;
+            else isMoving = false;
+        }
+    }
 }
