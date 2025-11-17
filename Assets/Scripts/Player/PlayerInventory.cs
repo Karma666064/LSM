f15606f5ec90b2f487be95dcf51e5625781480d3
Karma <72810712+Karma666064@users.noreply.github.com>
2025-11-17 18:08:47 +0400

    feat: Mini inventory done!

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,16 +1,50 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class InventoryObject
+{
+    public int id;
+    public enum Name { Paper }
+    public Name name;
+    public int count;
+}
 
 public class PlayerInventory : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<InventoryObject> inventory = new List<InventoryObject>();
+
+    public InventoryObject GetObject(InventoryObject.Name name)
     {
-        
+        InventoryObject obj = inventory.Find(item => item.name == name);
+
+        if (obj != null) return obj;
+        else return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    //public int GetCountOfObjectByName(InventoryObject.Name name)
+    //{
+    //    InventoryObject obj = GetObject(name);
+
+    //    return obj != null ? obj.count : 0;
+    //}
+
+    public void AddObject(InventoryObject.Name name, int number)
     {
-        
+        InventoryObject obj = GetObject(name);
+
+        if (obj != null) obj.count += number;
+        else inventory.Add(new InventoryObject { id = inventory.Count, name = name, count = number });
     }
+
+    //public void RemoveObject(InventoryObject.Name name, int number)
+    //{
+    //    InventoryObject obj = GetObject(name);
+
+    //    if (obj != null)
+    //    {
+    //        if (obj.count > 0 && obj.count - number > 0) obj.count -= number;
+    //        else inventory.Remove(obj);
+    //    }
+    //}
 }
